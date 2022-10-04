c04b41514e2c41e70ef9fe46b145884b250c01f1
MahekKamani <mahekkamani2001@gmail.com>
2022-10-04 13:53:25 +0530

    added password and policy discription to login page

--- src/Login.js
+++ src/Login.js
@@ -13,6 +13,11 @@ function Login() {
             <form>
                 <h5>Email</h5>
                 <input type="text"/>
+                <h5>Password</h5>
+                <input type="password"/>
+                <button>Sign In</button>
+                <p>By signing in you agree to the terms and conditions of use and sale of the company, Please see our Privacy Notice, Cookies Notice and Privacy bsed AD Notice</p>
+                <button>Create a new Account</button>
             </form>
         </div>
     </div>
