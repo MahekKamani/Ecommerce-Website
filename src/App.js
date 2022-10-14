a3a860c153a863085174d503da8644ace3dbb9de
MahekKamani <mahekkamani2001@gmail.com>
2022-10-14 21:24:16 +0530

    Added an event listner to keep track on the user who just loged In

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from './Header.js';
 import Home from './Home.js';
 import Checkout from './Checkout';
 import Login from './Login.js'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { auth } from './firebase';
+import { useStateValue } from './StateProvider';
+
 function App() {
+
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(authUser => {
+      console.log('The user is -> ', authUser);
+      if(authUser){
+        dispatch({
+          type: 'SET_USER',
+          user: authUser 
+        })
+      } else {
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+  },[])
+
   return (
     <Router>
       <div className="App">
