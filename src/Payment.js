eb02a5a986ad2f9055ade0b99620de591e89cdff
MahekKamani <mahekkamani2001@gmail.com>
2022-11-05 20:25:44 +0530

    resolved react-currency-format

--- src/Payment.js
+++ src/Payment.js
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { useState }from 'react'
 import CheckoutProduct from './CheckoutProduct'
 import './Payment.css'
 import { useStateValue } from './StateProvider'
 import { Link } from "react-router-dom"
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js"
- 
+import CurrencyFormat from "react-currency-format"
+import { getBasketTotal } from "./reducer"
+
 function Payment() {
   const [{basket, user},dispatch] = useStateValue(); 
   
   const stripe = useStripe();
   const elements = useElements();
+
+  const [error, setError] = useState(null);
+  const [disabled, setDisabled] = useState(true);
+
+  const handleSubmit = e => {
+
+  }
+
+  const handleChange = event => {
+    setDisabled(event.empty);
+    setError(event.error ? event.error.message : "");
+  }
 
   return (
     <div className='payment'>
@@ -50,7 +64,23 @@ function Payment() {
                     <h3>Payment Method</h3>
                 </div>
                 <div className='payment_details'>
-                    
+                    <form onSubmit={handleSubmit}>
+                        <CardElement onChange={handleChange}/>
+                    </form>
+                    <div className='payment_priceContainer'>
+                        <CurrencyFormat
+                        renderText={(value) => (
+                                <>
+                                <h3>Order Total : {value}</h3>
+                                </>
+                            )}
+                            decimalScale={2}
+                            value={getBasketTotal(basket)}
+                            displayType={"text"}
+                            thousandSeparator={true}
+                            prefix={"$"}
+                        />
+                    </div>
                 </div>
             </div>
         </div>
--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import "./Subtotal.css";
-import { useStateValue } from "./StateProvider";
-import { getBasketTotal } from './reducer';
-import { useNavigate } from 'react-router-dom';
+import React from 'react'
+import "./Subtotal.css"
+import CurrencyFormat from "react-currency-format"
+import { useStateValue } from "./StateProvider"
+import { getBasketTotal } from './reducer'
+import { useNavigate } from 'react-router-dom'
 
 function Subtotal() {
   const navigate = useNavigate();
@@ -12,6 +13,9 @@ function Subtotal() {
   const [ {basket} , dispatch] = useStateValue();
   return (
     <div className="subtotal">
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
                 <p>
                     Subtotal ({basket.length} items):
                     <strong>{getBasketTotal(basket)}</strong>
@@ -20,6 +24,14 @@ function Subtotal() {
                 <input type="checkbox" /> This order
                 contains a gift
                 </small>
+          </>
+        )}
+        decimalScale={2}
+        value={getBasketTotal(basket)}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"$"}
+        />
     <button onClick={handleClick}>Proceed to Checkout</button>
     </div>
   )
